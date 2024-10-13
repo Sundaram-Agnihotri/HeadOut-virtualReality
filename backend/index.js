ac65078d242bc210d330a5c153ae704c163c3456
Sundaram-Agnihotri <agnihotrisundaram8@gmail.com>
2024-10-13 19:47:57 +0530

    mongod intergration and writing the backend logic

--- backend/index.js
+++ backend/index.js
@@ -17,3 +17,52 @@ cloudinary.config({
   api_key: process.env.API_KEY,
   api_secret: process.env.API_SECRET,
 });
+
+// Log the configuration
+console.log(cloudinary.config());
+
+dotenv.config();
+
+const app = express();
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+app.use(cors());
+app.use(morgan("dev"));
+
+app.use(express.json({ limit: "10mb" }));
+app.use(express.urlencoded({ limit: "10mb", extended: true }));
+
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use(express.static("dist", { dotfiles: "allow" }));
+
+/* ROUTES */
+app.use("/auth", authRoutes);
+app.use("/media", mediaRoutes);
+
+app.get("/api", (req, res) => {
+  res.send("API is running");
+});
+
+app.get("*", (req, res) => {
+  res.sendFile(__dirname + "/dist/index.html");
+});
+
+const CONNECTION_URL = `mongodb+srv://${
+  process.env.DB_USERNAME
+}:${encodeURIComponent(
+  process.env.DB_PSWD
+)}@cluster.lczmihh.mongodb.net/headout?retryWrites=true&w=majority`;
+const PORT = process.env.PORT || 6001;
+mongoose
+  .connect(CONNECTION_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    app.listen(PORT, () => console.log(`SERVER PORT: ${PORT}`));
+  })
+  .catch((error) => console.log(`${error} did not connect`));
+
+  export default app;
